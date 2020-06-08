58372a5db6316b5adc1488c131669e2ae8634f28
andrei_dev_wear <making714@gmail.com>
2020-06-09 02:27:33 +0300

    added different planet random view, added sun material

---
+++ Assets/Scripts/Planet/RandomTexture/NoiseMap.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public enum MapType
+{
+    Noise,
+    Color
+}
+
+public class NoiseMap : MonoBehaviour
+{
+    [SerializeField] private NoiseMapRenderer _mapRenderer;
+    // Input data for our noise generator
+    [SerializeField] public int width;
+    [SerializeField] public int height;
+    [SerializeField] public float scale;
+
+    [SerializeField] public int octaves;
+    [SerializeField] public float persistence;
+    [SerializeField] public float lacunarity;
+
+    [SerializeField] public Vector2 offset;
+
+    [SerializeField] public MapType type = MapType.Noise;
+
+    private NoiseMapGenerator _noiseMapGenerator;
+
+    private void Start()
+    {
+        _noiseMapGenerator = new NoiseMapGenerator();
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        // Generate a map
+        float[] noiseMap = _noiseMapGenerator.GenerateNoiseMap(width, height, scale, octaves, persistence, lacunarity, offset);
+
+        // Pass the map to the renderer
+        _mapRenderer.RenderMap(width, height, noiseMap, type);
+    }
+}
---
+++ Assets/Scripts/Planet/RandomTexture/RandomTexture.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTexture : MonoBehaviour
+{
+    [SerializeField]private int _pixWidth;
+    [SerializeField] private int _pixHeight;
+    [SerializeField] private float _xOrg;
+    [SerializeField] private float _yOrg;
+    [SerializeField] private float _scale = 1.0F;
+
+    private Texture2D _noiseTex;
+    private Color[] _pix;
+    private Renderer _rend;
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+
+        // Set up the texture and a Color array to hold pixels during processing.
+        _noiseTex = new Texture2D(_pixWidth, _pixHeight);
+        _pix = new Color[_noiseTex.width * _noiseTex.height];
+        _rend.material.mainTexture = _noiseTex;
+    }
+
+    void CalcNoise()
+    {
+        // For each pixel in the texture...
+        float y = 0.0F;
+
+        while (y < _noiseTex.height)
+        {
+            float x = 0.0F;
+            while (x < _noiseTex.width)
+            {
+                float xCoord = _xOrg + x / _noiseTex.width * _scale;
+                float yCoord = _yOrg + y / _noiseTex.height * _scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                _pix[(int)y * _noiseTex.width + (int)x] = new Color(sample, sample, sample);
+                x++;
+            }
+            y++;
+        }
+
+        // Copy the pixel data to the texture and load it into the GPU.
+        _noiseTex.SetPixels(_pix);
+        _noiseTex.Apply();
+    }
+
+    void Update()
+    {
+        CalcNoise();
+    }
+}
