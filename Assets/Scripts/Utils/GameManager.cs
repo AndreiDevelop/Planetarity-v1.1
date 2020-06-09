a77b184caecd6b82feeaa0881910a9de16b231bb
andrei_dev_wear <making714@gmail.com>
2020-06-09 03:21:45 +0300

    fixed gameplay balance, added Pause/resume feature

---
+++ Assets/Scripts/Utils/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _gameIsPaused = false;
+
+    [System.Obsolete]
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SetPauseUnpauseGame();
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+    }
+
+    private void SetPauseUnpauseGame()
+    {
+        _gameIsPaused = !_gameIsPaused;
+        Time.timeScale = (_gameIsPaused) ? 0 : 1;
+    }
+
+    [System.Obsolete]
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
